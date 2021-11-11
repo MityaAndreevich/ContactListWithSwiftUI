715def46e48c874fa7256ac188b1b619ef11710e
MityaAndreevich <dmitry.logachev@icloud.com>
2021-11-11 19:31:50 +0300

    Initial Commit

---
+++ ContactListWithSwiftUI/ContactListWithSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactListWithSwiftUIApp.swift
+//  ContactListWithSwiftUI
+//
+//  Created by Dmitry Logachev on 11.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct ContactListWithSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
